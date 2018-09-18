bfa61969d799d5cbba4b41ba00ad6aff4ec0a027
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 09:34:51 -0500

    reference count function

--- SmartPointerClass/main.cpp
+++ SmartPointerClass/main.cpp
@@ -1,1 +1,22 @@
-
+#include <string>
+#include <iostream>
+using namespace std;
+/* Reference Count: A simple class for maanging the number of active smart pointers*/
+class ReferenceCount
+{
+private:
+    int m_RefCount{ 0 };
+public:
+    void Increment()
+    {
+        ++m_RefCount;
+    }
+    int Decrement()
+    {
+        return --m_RefCount;
+    }
+    int GetCount() const
+    {
+        return m_RefCount;
+    }
+};
